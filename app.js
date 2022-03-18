22dc3b0e8a110d7eb80b5fd9d42d5d87e7e0d81e
asadbeksatimov <b200109576@subu.edu.tr>
2022-03-18 13:58:03 +0300

    Words in the English dictionary

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-var sayi1=document.getElementById("s1");
-var sayi2=document.getElementById("s2");
-var hesapBtnPlus =document.getElementById("hesapplus");
-var hesapBtnMinus=document.getElementById("hesapayirma");
-var hesapBtnCarpma=document.getElementById("hesapcarpma");
-var hesapBtnBolme=document.getElementById("hesapbolme");
-var hesapBtnFaiz=document.getElementById("hesapfaiz");
-var sonuc=document.getElementById("sonuc");
+var num1=document.getElementById("n1");
+var num2=document.getElementById("n2");
+var toCalculateAdd =document.getElementById("add");
+var toCalculateSeparate=document.getElementById("separate");
+var toCalculateMultiply=document.getElementById("multiply");
+var toCalculatePartition=document.getElementById("partition");
+var toCalculatePercent=document.getElementById("percent");
+var sonuc=document.getElementById("collection");
 
-hesapBtnPlus.onclick=function(){
-    var toplam= Number(sayi1.value) + Number(sayi2.value);
-    sonuc.innerHTML=toplam;
+toCalculateAdd.onclick=function(){
+    var toplam= Number(num1.value) + Number(num2.value);
+    collection.innerHTML=toplam;
 }
-hesapBtnMinus.onclick=function(){
-    var toplam= Number(sayi1.value) - Number(sayi2.value);
-    sonuc.innerHTML=toplam;
+toCalculateSeparate.onclick=function(){
+    var toplam= Number(num1.value) - Number(num2.value);
+    collection.innerHTML=toplam;
 }
-hesapBtnCarpma.onclick=function(){
-    var toplam= Number(sayi1.value) * Number(sayi2.value);
-    sonuc.innerHTML=toplam;
+toCalculateMultiply.onclick=function(){
+    var toplam= Number(num1.value) * Number(num2.value);
+    collection.innerHTML=toplam;
 }
-hesapBtnBolme.onclick=function(){
-    var toplam= Number(sayi1.value) / Number(sayi2.value);
-    sonuc.innerHTML=toplam;
+toCalculatePartition.onclick=function(){
+    var toplam= Number(num1.value) / Number(num2.value);
+    collection.innerHTML=toplam;
 }
-hesapBtnFaiz.onclick=function(){
-    var toplam= Number(sayi1.value) / Number(sayi2.value);
-    sonuc.innerHTML=toplam;
+toCalculatePercent.onclick=function(){
+    var toplam= Number(num1.value) / Number(num2.value);
+    collection.innerHTML=toplam;
 }
